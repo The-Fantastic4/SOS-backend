86dc1297d1a49b1246c21b93f6eec32f8793583a
CpulsiveK <impulsivecliff7@gmail.com>
2023-11-07 18:07:13 +0100

    fixed path error in location processing route

--- src/routes/locationProcessingRoute.ts
+++ src/routes/locationProcessingRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import getClosestPoliceStationLocation from "../controllers/locationProcessingController";
+import { getClosestPoliceStationLocation } from "../controllers/locationProcessingController";
 
 const router = express.Router();
 
