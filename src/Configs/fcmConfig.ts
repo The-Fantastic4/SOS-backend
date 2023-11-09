904b94fda41d13931941149035cccd6a629b76b8
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-11-09 12:13:54 +0100

    Delete src/Configs directory

--- src/Configs/fcmConfig.ts
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin"
-
-var serviceAccount = "./sos-app-6bd86-firebase-adminsdk-ewfsq-0ce893fb9f.json"
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
-
-export const messaging = admin.messaging()
